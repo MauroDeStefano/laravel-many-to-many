9af18191eb6c745691128d6c6c004a8bfacd27a3
MauroDeStefano <maurodestefano@gmail.com>
2022-02-09 18:17:49 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('prova', function(){
+    $data = [
+        'Mauro',
+        'De Stefano'
+    ];
+
+    return response()->json(compact('data'));
 });
+
+Route::get('posts', 'Api\PostController@index');
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-})->name('home');
 
 Auth::routes();
 
@@ -28,3 +25,6 @@ Route::middleware('auth')
         Route::resource('/posts' , 'PostController');
     });
 
+Route::get('{any?}', function () {
+    return view('guest.home');
+})->where('any', '*')->name('home');
